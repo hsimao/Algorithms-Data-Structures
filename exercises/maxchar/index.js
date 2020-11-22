b352f1667f03655691af4ed74d9971783c4ec363
hsimao <e087754958@gmail.com>
2020-11-22 15:55:41 +0800

    add: max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // const strMap = str.split("").reduce((acc, it) => {
+  //   acc[it] = acc[it] + 1 || 1;
+  //   return acc;
+  // }, {});
+
+  const strMap = {};
+  for (let val of str) {
+    strMap[val] = strMap[val] + 1 || 1;
+  }
+
+  let maxNum = 0;
+  let maxStr = "";
+  for (let key in strMap) {
+    if (strMap[key] > maxNum) {
+      maxStr = key;
+      maxNum = strMap[key];
+    }
+  }
+
+  return maxStr;
+}
 
 module.exports = maxChar;
