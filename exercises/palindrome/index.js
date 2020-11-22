62508d008a71c3c8fd6a521e0386d0bafd47a560
hsimao <e087754958@gmail.com>
2020-11-22 15:20:51 +0800

    add: palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // == 解法 1
+  // const halfNum = Math.floor(str.length / 2);
+  // const firstStr = str.substr(0, halfNum);
+  // let lastStr = str.substr(halfNum, str.length);
+  // if (firstStr.length !== lastStr.length) {
+  //   lastStr = lastStr.substr(1, lastStr.length);
+  // }
+  // if (lastStr === firstStr.split("").reverse().join("")) {
+  //   return true;
+  // }
+  // return false;
+
+  // == 解法 2
+  // return str === str.split("").reverse().join("");
+
+  // == 解法 3
+  return str.split("").every((char, i) => {
+    return char === str[str.length - 1 - i];
+  });
+}
 
 module.exports = palindrome;
