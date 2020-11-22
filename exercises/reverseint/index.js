151ef61155f45fe8c9c48123f09983fee291aada
hsimao <e087754958@gmail.com>
2020-11-22 15:22:04 +0800

    add: reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// 解法 1
+// function reverseInt(n) {
+//   const negative = Math.sign(n);
+//   let reverse = n.toString().split("").reverse().join("");
+
+//   if (negative === -1) {
+//     reverse = "-" + reverse.replace("-", "");
+//   }
+
+//   return parseInt(n);
+// }
+
+// 解法 2
+function reverseInt(n) {
+  const reverse = n.toString().split("").reverse().join("");
+  if (n < 0) return reverse * Math.sign(n); // Math.sign(), 負數會回傳 -1, 正數回傳 1
+  return reverse;
+}
 
 module.exports = reverseInt;
