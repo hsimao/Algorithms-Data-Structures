a2523a63482ff82949e8c34f5ea88aeb4dcce87b
hsimao <e087754958@gmail.com>
2020-11-23 21:10:42 +0800

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// 解法 1
+// function anagrams(stringA, stringB) {
+//   function makeObj(array) {
+//     return array.reduce((acc, it) => {
+//       acc[it] = acc[it] + 1 || 1;
+//       return acc;
+//     }, {});
+//   }
+
+//   function formatAndSplit(str) {
+//     return str.replace(/[^\w]/g, "").toLowerCase().split("");
+//   }
+
+//   const objA = makeObj(formatAndSplit(stringA));
+//   const objB = makeObj(formatAndSplit(stringB));
+
+//   if (Object.keys(objA).length !== Object.keys(objB).length) {
+//     return false;
+//   }
+
+//   for (let key in objA) {
+//     if (objA[key] !== objB[key]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// 解法 2
+function anagrams(stringA, stringB) {
+  function formatAndSplit(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+  }
+
+  return formatAndSplit(stringA) === formatAndSplit(stringB);
+}
 
 module.exports = anagrams;
