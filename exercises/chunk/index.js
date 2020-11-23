0ef339e7aaecfc5fa6f68684bae5d9ff5ebf85a5
hsimao <e087754958@gmail.com>
2020-11-23 19:48:48 +0800

    add: chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,33 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// 解法 1
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let val of array) {
+//     // 取出 chunked 最後一個陣列
+//     const last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       // 產生新的 subArray
+//       chunked.push([val]);
+//     } else {
+//       // push 到 chunked 關聯的最後一個陣列內
+//       last.push(val);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+// 解法 2
+function chunk(array, size) {
+  const chunked = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunked.push(array.slice(i, i + size));
+  }
+  return chunked;
+}
 
 module.exports = chunk;
