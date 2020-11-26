042cc0228a8ee40673e5b14da3f17cddd7bae9cf
hsimao <e087754958@gmail.com>
2020-11-26 20:52:50 +0800

    add: steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,49 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// 解法 1
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     const mark = i;
+//     const space = n - i;
+//     let print = "";
 
+//     for (let m = 1; m <= mark; m++) {
+//       print += "#";
+//     }
+
+//     for (let s = 1; s <= space; s++) {
+//       print += " ";
+//     }
+//     console.log(print);
+//   }
+// }
+
+// 解法 2
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+// 解法 3 使用遞歸 Recursion
+function steps(n, row = 0, stair = "") {
+  if (n === row) return;
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? "#" : " ";
+  return steps(n, row, stair + add);
+}
 module.exports = steps;
