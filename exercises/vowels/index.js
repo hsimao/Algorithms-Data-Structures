f7ef529608c60fb2abb5b5d0ac60ae5142db0b97
hsimao <e087754958@gmail.com>
2020-11-28 20:26:14 +0800

    add: vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,38 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// 解法 1
+// function vowels(str) {
+//   const vowelsChar = "aeiou";
+//   return str
+//     .toLowerCase()
+//     .split("")
+//     .reduce((acc, it) => {
+//       if (vowelsChar.includes(it)) {
+//         acc++;
+//       }
+//       return acc;
+//     }, 0);
+// }
+
+// 解法 2
+// function vowels(str) {
+//   const vowelsArray = ["a", "e", "i", "o", "u"];
+//   let count = 0;
+
+//   for (const char of str.toLowerCase()) {
+//     if (vowelsArray.includes(char)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// 解法 3 使用正規表達式
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
