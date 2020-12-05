bbe73118027f3de9dc4127af29c9af3f502686bc
hsimao <e087754958@gmail.com>
2020-12-05 16:28:43 +0800

    add: linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,91 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constrructor() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) return null;
+
+    let node = this.head;
+    while (node) {
+      if (!node.next) return node;
+      node = node.next;
+    }
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      return (last.next = new Node(data));
+    }
+    this.head = new Node(data);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) return;
+    if (!this.head.next) return (this.head = null);
+
+    let node = this.head.next;
+    let prevNode = this.head;
+    while (node.next) {
+      prevNode = node;
+      node = node.next;
+    }
+    prevNode.next = null;
+  }
+
+  getAt(index) {
+    let count = 0;
+    let node = this.head;
+
+    while (node) {
+      if (index === count) return node;
+
+      count++;
+      node = node.next;
+    }
+    return null;
+  }
+}
 
 module.exports = { Node, LinkedList };
