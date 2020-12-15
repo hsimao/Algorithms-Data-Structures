3c297938a4a452806e80c32553be89966c722c26
hsimao <e087754958@gmail.com>
2020-12-15 20:24:17 +0800

    add: Binary Search Trees

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,52 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+
+  // 全部過濾判斷方法, 效率較差
+  // contains(data) {
+  //   let array = [this];
+  //   while (array.length) {
+  //     const node = array.shift();
+  //     if (node.data === data) {
+  //       return node;
+  //     } else {
+  //       if (node.left) array.push(node.left);
+  //       if (node.right) array.push(node.right);
+  //     }
+  //   }
+  //   return null;
+  // }
+
+  // 依據資料大小, 只單純跑吻合的路線
+  contains(data) {
+    if (this.data === data) return this;
+
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+
+    return null;
+  }
+}
 
 module.exports = Node;
