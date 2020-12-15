9a3b257974441f7ecf752cc047bf13a577450bf4
hsimao <e087754958@gmail.com>
2020-12-15 19:38:56 +0800

    add: tree levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const array = [root, "s"];
+  const counter = [0];
+  while (array.length > 1) {
+    const node = array.shift();
+    if (node === "s") {
+      counter.push(0);
+      array.push("s");
+    } else {
+      array.push(...node.children);
+      counter[counter.length - 1]++;
+    }
+  }
+  return counter;
+}
 
 module.exports = levelWidth;
