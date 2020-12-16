65e1b9967983501a612c449fc0b692276c0b17e0
hsimao <e087754958@gmail.com>
2020-12-16 21:27:34 +0800

    add: BubbleSort and SelectionSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,68 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j + 1];
+//         arr[j + 1] = arr[j];
+//         arr[j] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
+// ES2015 陣列互換方法
+// function bubbleSort(arr) {
+//   let counter = 0;
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       counter++;
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   console.log("counter: ", counter);
+//   return arr;
+// }
+
+// 優化迴圈次數, 如果第二層迴圈已經有一次都沒有需要交換的, 將退出循環
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    let flag = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        flag = false;
+      }
+    }
+    if (flag) break;
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      [arr[i], arr[indexOfMin]] = [arr[indexOfMin], arr[i]];
+    }
+  }
+  return arr;
 }
 
-function mergeSort(arr) {
+function mergeSort(arr) {}
 
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
