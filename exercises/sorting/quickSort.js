57d2e5b6724d82152ca2e452619289f8dd45dce3
hsimao <e087754958@gmail.com>
2020-12-20 16:33:39 +0800

    add: Quick Sort

---
+++ exercises/sorting/quickSort.js
@@ -0,0 +1,32 @@
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+function pivot(arr, start = 0, end = arr.length + 1) {
+  let pivot = arr[start];
+  let swapIndex = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+
+  swap(arr, start, swapIndex);
+
+  return swapIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    const pivotIndex = pivot(arr, left, right);
+    // left
+    quickSort(arr, left, pivotIndex - 1);
+    // right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+module.exports = { quickSort, pivot };
--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -1,8 +1,11 @@
-const S = require('./index');
+const S = require("./index");
 const bubbleSort = S.bubbleSort;
 const selectionSort = S.selectionSort;
 const mergeSort = S.mergeSort;
 const merge = S.merge;
+const Q = require("./quickSort");
+const quickSort = Q.quickSort;
+const pivot = Q.pivot;
 
 function getArray() {
   return [100, -40, 500, -124, 0, 21, 7];
@@ -12,27 +15,36 @@ function getSortedArray() {
   return [-124, -40, 0, 7, 21, 100, 500];
 }
 
-describe('Bubble sort', () => {
-  test('sorts an array', () => {
+describe("Bubble sort", () => {
+  test("sorts an array", () => {
     expect(bubbleSort(getArray())).toEqual(getSortedArray());
   });
 });
 
-describe('Selection sort', () => {
-  test('sorts an array', () => {
+describe("Selection sort", () => {
+  test("sorts an array", () => {
     expect(selectionSort(getArray())).toEqual(getSortedArray());
   });
 });
 
-describe('Merge sort', () => {
-  test('merge function can join together two sorted arrays', () => {
+describe("Merge sort", () => {
+  test("merge function can join together two sorted arrays", () => {
     const left = [1, 10];
     const right = [2, 8, 12];
 
-    expect(merge(left, right)).toEqual([1,2,8,10,12]);
+    expect(merge(left, right)).toEqual([1, 2, 8, 10, 12]);
   });
-  
-  test('sorts an array', () => {
+
+  test("sorts an array", () => {
     expect(mergeSort(getArray())).toEqual(getSortedArray());
+  });
+});
+
+describe("Quick sort", () => {
+  test("pivot sort", () => {
+    expect(pivot([4, 8, 2, 1, 5, 7, 6, 3])).toEqual(3);
+  });
+  test("Quick sort", () => {
+    expect(quickSort(getArray())).toEqual(getSortedArray());
   });
 });
